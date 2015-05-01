c9b949e1b452ba51d6ebd7b1697a52ccbd279938
Thy <ThyRex@users.noreply.github.com>
2015-04-30 19:31:26 -0500

    Update backend.php

--- backend.php
+++ backend.php
@@ -1,27 +1,25 @@
 <?php
 $incoming = file_get_contents('php://input');
-// echo $incoming ."<br>";
+// If incoming is an option selected, then use it as the
+// goto directory
 if($incoming != null) {
-	$useThis = $incoming;
-}
-if(isset($useThis)){
-	$cur_dir = $useThis;
+	$cur_dir = $incoming;
 }
 else{
+	// otherwise, the current directory is whereever
+	// we're working in
+	// This means we've just opened the page
 	$cur_dir = getcwd();
 }
 
-// echo $cur_dir . "<br>";
-// chdir($cur_dir . "/assignment7");
-// $cur_dir = getcwd();
-// echo $cur_dir;
-$ob2 = new stdClass();
-$ob2->dir = $cur_dir;
-$ob2->folders = glob($cur_dir . "/*", GLOB_ONLYDIR);
-$ob2->files = array_filter(glob($cur_dir . "/*"), 'is_file');
+
+// Code from Gashler
+$ob = new stdClass();
+$ob->dir = $cur_dir;
+// Code from forum
+$ob->files = array_filter(glob($cur_dir . "/*"), 'is_file');
+$ob->folders = glob($cur_dir . "/*", GLOB_ONLYDIR);
 
 header('Content-Type: application/json');
-// $ob3 = json_encode($ob2);
-// echo $ob3 ."<br>";
-print(json_encode($ob2));
+print(json_encode($ob));
 ?>
